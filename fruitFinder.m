64f8d6cb2831245ba73f518036ac6571557fd751
Eric Henderson <henderea@rose-hulman.edu>
2012-12-07 14:54:05 -0500

    improving orange mask

--- fruitFinder.m
+++ fruitFinder.m
@@ -2,14 +2,14 @@
 
 % function [fruit_mask fruit_count fruit_centroids] = fruitFinder(img)
 function [fruit_mask] = fruitFinder(img)
-neg_filters = [0 1 0.275 0.425 0.1 0.45 false;
+neg_filters = [0.0 1.0 0.275 0.425 0.1 0.45 false;
                0.15 0.45 0.0 0.9 0.0 0.225 false;
                0.1 0.275 0.125 0.35 0.0 1.0 false;
               ];
 
 banana_filter = [0.125 0.225 0.65 0.8 0.6 1.0 false];
-apple_filter = [0.925 0.1 0.6 1.01 0.05 0.5 false];
-orange_filter = [0.0775 0.1071 0.8107 0.9152 0.5977 0.8256 false];
+apple_filter = [0.925 0.1 0.6 1.0 0.05 0.5 false];
+orange_filter = [0.06 0.11 0.6 1.0 0.5 0.95 false];
 
 % Create the negative mask
 mask = ~makeMultiMaskFromRGB(img, neg_filters);
