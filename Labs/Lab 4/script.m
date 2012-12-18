7866a4f0a06b43b3a10e68822e9f0557cc7e6309
Nicholas Kamper <kampernj@rose-hulman.edu>
2012-12-18 15:29:50 -0500

    Added elongation
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

--- Labs/Lab 4/script.m
+++ Labs/Lab 4/script.m
@@ -3,3 +3,8 @@ img = imread('images/shapes.png');
 
 % Run bwlabel and split into separate masks for each shape
 shapes = createRegionMasks(bwlabel(img, 4));
+
+% Calculate and print the results for elongation and circularity
+for i=1:size(shapes, 3)
+    elong = calculateElongation(shapes(:,:,i));
+end
