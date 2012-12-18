a0a64bf5ca8ede4a21303cc9f1b5f757b4f50886
Nicholas Kamper <kampernj@rose-hulman.edu>
2012-12-18 16:07:46 -0500

    Cleaned up the script
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

--- Labs/Lab 4/script.m
+++ Labs/Lab 4/script.m
@@ -4,7 +4,13 @@ img = imread('images/shapes.png');
 % Run bwlabel and split into separate masks for each shape
 shapes = createRegionMasks(bwlabel(img, 4));
 
+elong = zeros(size(shapes, 3), 1);
+c1 = zeros(size(shapes, 3), 1);
+c2 = zeros(size(shapes, 3), 1);
+
 % Calculate and print the results for elongation and circularity
 for i=1:size(shapes, 3)
-    elong = calculateElongation(shapes(:,:,i));
+    elong(i) = calculateElongation(shapes(:,:,i));
+    [c1(i) c2(i)] = getCircularity(shapes(:,:,i));
+    fprintf('Shape %d: Elongation: %f; C1: %f; C2: %f\n', i, elong(i), c1(i), c2(i));
 end
