6056c647d9ddbba8b5e3b7467e74749cb78578dc
Nicholas Kamper <kampernj@rose-hulman.edu>
2013-01-16 15:53:13 -0500

    stuff
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

--- SunsetDetector/runSVMTest.m
+++ SunsetDetector/runSVMTest.m
@@ -1,5 +1,5 @@
 function [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = runSVMTest(xTrain, yTrain, xTest, yTest, kernel, param)
-    net = svm(size(xTrain, 2), kernel, [param], 10);
+    net = svm(size(xTrain, 2), kernel, param, 10);
     net = svmtrain(net, xTrain, yTrain);
     
     testResults = svmfwd(net, xTest);
