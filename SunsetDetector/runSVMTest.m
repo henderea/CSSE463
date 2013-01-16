c3f7b40174b841b61af39b99417f1241a8617b52
Eric Henderson <henderea@rose-hulman.edu>
2013-01-16 17:10:15 -0500

    use normalized y1 for classification

--- SunsetDetector/runSVMTest.m
+++ SunsetDetector/runSVMTest.m
@@ -2,6 +2,9 @@ function [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = runSVMTest(xTrain, yTrain, x
     net = svm(size(xTrain, 2), kernel, param, 10);
     net = svmtrain(net, xTrain, yTrain);
     
-    testResults = svmfwd(net, xTest);
-    [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = gatherStatistics(testResults, yTest);
+    [testResults,y1] = svmfwd(net, xTest);
+    y1b = (y1 - repmat(mean(y1), size(y1,1), size(y1, 2))) / std(y1);
+    y1c = ones(size(y1b));
+    y1c(y1b < 0) = -1;
+    [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = gatherStatistics(y1c, yTest);
 end
