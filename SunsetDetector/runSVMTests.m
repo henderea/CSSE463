4bc325f8121ad499d727836f2304d5cd04303138
Eric Henderson <henderea@rose-hulman.edu>
2013-01-17 15:03:02 -0500

    fixing runSVMTests

--- SunsetDetector/runSVMTests.m
+++ SunsetDetector/runSVMTests.m
@@ -1,8 +1,9 @@
 function [kernel, param] = runSVMTests(xTrain, yTrain, xTest, yTest, polyparams, rbfparams)
     amax = 0;
     ctest = size(xTest, 1);
+    [oxTrain, oxTest] = normalizeVectors(xTrain, xTest);
     for(i = 1:size(polyparams,2))
-        [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = runSVMTest(xTrain, yTrain, xTest, yTest, 'poly', polyparams(i));
+        [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = runSVMTest(oxTrain, yTrain, oxTest, yTest, 'poly', polyparams(i));
         fprintf('Kernel: poly; Param: %.3f\n', polyparams(i));
         fprintf('True positive: %d/%d; True negative: %d/%d; False positive: %d/%d; False negative: %d/%d\n', ctp, ctest, ctn, ctest, cfp, ctest, cfn, ctest);
         fprintf('TPR: %.2f%%; FPR: %.3f%%; Accuracy: %.3f%%\n\n', tpr, fpr, accuracy);
@@ -13,14 +14,14 @@ function [kernel, param] = runSVMTests(xTrain, yTrain, xTest, yTest, polyparams,
         end
     end
     for(i = 1:size(rbfparams,2))
-        [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = runSVMTest(xTrain, yTrain, xTest, yTest, 'rbf', rbfparams(i));
+        [ctp, ctn, cfp, cfn, tpr, fpr, accuracy] = runSVMTest(oxTrain, yTrain, oxTest, yTest, 'rbf', rbfparams(i));
         fprintf('Kernel: rbf; Param: %.3f\n', rbfparams(i));
         fprintf('True positive: %d/%d; True negative: %d/%d; False positive: %d/%d; False negative: %d/%d\n', ctp, ctest, ctn, ctest, cfp, ctest, cfn, ctest);
         fprintf('TPR: %.2f%%; FPR: %.3f%%; Accuracy: %.3f%%\n\n', tpr, fpr, accuracy);
         if(accuracy > amax)
             amax = accuracy;
             kernel = 'rbf';
-            param = polyparams(i);
+            param = rbfparams(i);
         end
     end
 end
