2d2382360de64e39ba242a7c662b296a1c060431
Nicholas Kamper <kampernj@rose-hulman.edu>
2013-01-18 14:23:52 -0500

    stuff
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

--- Labs/Lab 6/calculateDistance.m
+++ Labs/Lab 6/calculateDistance.m
@@ -1,9 +1,7 @@
 function dist = calculateDistance(a, b)
     dist = 0;
-    size(a)
-    size(b)
     for i=1:size(a, 2)
         dist = dist + ((b(i) - a(i)) * (b(i) - a(i)));
     end
-    dist = sqrt(dist);
+    dist = sqrt(double(dist));
 end
--- Labs/Lab 6/performKMeans.m
+++ Labs/Lab 6/performKMeans.m
@@ -1,8 +1,8 @@
 function [clusters, ptassignments] = performKMeans(k, points, iterations, seed)
     % Initialize the clusters
     rand('state', seed);
-    clusters = rand(k,4); 
-    ptassignments = -ones(2, size(points,1));
+    clusters = rand(k,3); 
+    ptassignments = -ones(size(points, 1), size(points, 2));
     
     % For the number of iterations...
     for i=1:iterations
@@ -10,17 +10,30 @@ function [clusters, ptassignments] = performKMeans(k, points, iterations, seed)
             for pt_y=1:size(points, 2)
                 point = points(pt_x, pt_y, :);
                 closest_cluster = 1;
-                closest_dist = calculateDistance(clusters(:,:,:, 1), point(1,1,:));
+                closest_dist = calculateDistance(clusters(1, :), point(1,:));
                 for clust=2:k
-                    dist = calculateDistance(clusters(:,:,:, clust), point(1,1,:));
+                    dist = calculateDistance(clusters(clust, :), point(1,:));
                     if (dist < closest_dist)
                         closest_cluster = k;
                         closest_dist = dist;
                     end
                 end
+                ptassignments(pt_x, pt_y) = closest_cluster;
             end
-            
-            ptassignments(pt) = closest_cluster;
+        end
+        
+        % Recalculate the cluster means
+        for clust=1:k
+            idx = 1;
+            for pt_x=1:size(points, 1)
+                for pt_y=1:size(points, 2)
+                    if ptassignments(pt_x, pt_y) == clust
+                        cluster_points(idx) = points(pt_x, pt_y);
+                        idx = idx + 1;
+                    end
+                end
+            end
+            clusters(clust) = mean(cluster_points);
         end
     end
 end
