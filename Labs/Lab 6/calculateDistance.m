032b252cc965c4288dbf749e1791e9a471cf4ea7
Eric Henderson <henderea@rose-hulman.edu>
2013-01-22 16:37:24 -0500

    use norm function

--- Labs/Lab 6/calculateDistance.m
+++ Labs/Lab 6/calculateDistance.m
@@ -1,7 +1,3 @@
 function dist = calculateDistance(a, b)
-    dist = 0;
-    for i=1:size(a, 2)
-        dist = dist + ((b(i) - a(i)) * (b(i) - a(i)));
-    end
-    dist = sqrt(double(dist));
+    dist = norm(a - b);
 end
