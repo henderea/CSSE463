2539b496cf093192d30722e4ee0af9f3ac1b0eec
Eric Henderson <henderea@rose-hulman.edu>
2013-01-22 15:34:54 -0500

    actually commit k-means changes

--- Labs/Lab 6/performKMeans.m
+++ Labs/Lab 6/performKMeans.m
@@ -1,20 +1,20 @@
 function [clusters, ptassignments] = performKMeans(k, points, iterations, seed)
     % Initialize the clusters
     rand('state', seed);
-    clusters = rand(k,3); 
+    clusters = rand(k,3)*255; 
     ptassignments = -ones(size(points, 1), size(points, 2));
     
     % For the number of iterations...
     for i=1:iterations
         for pt_x=1:size(points, 1)
             for pt_y=1:size(points, 2)
-                point = points(pt_x, pt_y, :);
+                point = reshape(points(pt_x, pt_y, :), 1, 3);
                 closest_cluster = 1;
-                closest_dist = calculateDistance(clusters(1, :), point(1,:));
+                closest_dist = calculateDistance(clusters(1, :), point);
                 for clust=2:k
-                    dist = calculateDistance(clusters(clust, :), point(1,:));
+                    dist = calculateDistance(clusters(clust, :), point);
                     if (dist < closest_dist)
-                        closest_cluster = k;
+                        closest_cluster = clust;
                         closest_dist = dist;
                     end
                 end
@@ -25,15 +25,16 @@ function [clusters, ptassignments] = performKMeans(k, points, iterations, seed)
         % Recalculate the cluster means
         for clust=1:k
             idx = 1;
+            cluster_points = -ones(1,3);
             for pt_x=1:size(points, 1)
                 for pt_y=1:size(points, 2)
                     if ptassignments(pt_x, pt_y) == clust
-                        cluster_points(idx) = points(pt_x, pt_y);
+                        cluster_points(idx,:) = points(pt_x, pt_y, :);
                         idx = idx + 1;
                     end
                 end
             end
-            clusters(clust) = mean(cluster_points);
+            clusters(clust,:) = mean(cluster_points);
         end
     end
 end
