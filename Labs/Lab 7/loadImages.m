5985c9e06e4d0ce7ed0ab612937d5cd3302d5c1f
Nicholas Kamper <kampernj@rose-hulman.edu>
2013-01-25 14:15:52 -0500

    Added loadImages
    Signed-off-by: Nicholas Kamper <kampernj@rose-hulman.edu>

---
+++ Labs/Lab 7/loadImages.m
@@ -0,0 +1,14 @@
+function mtx = loadImages(globPath, px, scale)
+    files = dir(globPath);
+    mtx = zeros(px, size(files, 1) - 3);
+    for i=3:size(files, 1)
+        fn = files(i).name;
+        fn_path = [globPath filesep fn];
+        
+        img = imread(fn_path);
+        resized = imresize(img, 1/double(scale));
+        mtx(:,i-2) = reshape(resized, [], 1);
+
+    end
+end
+    
