dfd1d5ceba9b6c876abe6033525bca44061155b7
Eric Henderson <henderea@gmail.com>
2013-01-30 10:10:40 -0500

    scale to 0-255

--- Labs/Lab 7/prepareEignevectors.m
+++ Labs/Lab 7/prepareEignevectors.m
@@ -1,7 +1,7 @@
 function images = prepareEignevectors(eigvs, rows, cols)
     images = zeros(rows, cols, 3, size(eigvs, 2));
     for i=1:size(eigvs, 2)
-        images(:, :, :, i) = reshape(eigvs(:, i), rows, cols, 3)*255;
+        images(:, :, :, i) = reshape(eigvs(:, i), rows, cols, 3);
     end
-    images = uint8(images);
+    images = uint8(images*(255 / max(max(eigvs))));
 end
